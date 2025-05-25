67824e3a7b8dea4d05875160b05b7f3da5731266
MariamMohamedEid <mariiameidd@gmail.com>
2025-05-25 14:30:10 +0300

    modified the load.py

--- src/load.py
+++ src/load.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(filename='logs/etl.log', level=logging.INFO)
 
 def load(data):
-    with open('output.csv', 'w') as f:  # Create/overwrite 'output.csv'
+    with open('./output.csv', 'w') as f:  # Create/overwrite 'output.csv'
         writer = csv.writer(f)
         writer.writerow(['City', 'Count'])  # Write header row
         
